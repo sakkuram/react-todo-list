068274402775c31d85b3ae93c92a780c5d468891
[den] <114509752+sakkuram@users.noreply.github.com>
2023-11-13 23:35:00 +0200

    Update reportWebVitals.js

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -5,7 +5,7 @@ const reportWebVitals = onPerfEntry => {
       getFID(onPerfEntry);
       getFCP(onPerfEntry);
       getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);12345678901234
+      getTTFB(onPerfEntry);123456789012345
     });
   }
 };
