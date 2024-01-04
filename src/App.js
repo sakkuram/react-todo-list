6e8711156e7c84f8f5899669b5146dbee6dbf149
[den] <114509752+sakkuram@users.noreply.github.com>
2024-01-04 20:23:35 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import TodoWrapper from "./components/TodoWrapper";
 
 function App() {
   return (
-    <div className="App">1234512
+    <div className="App">12345123
       <TodoWrapper />
     </div>
   );
